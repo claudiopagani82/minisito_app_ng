6ef0d24676660fc18eddcdf528d46013c7f9a4d8
claudiopagani82 <150392104+claudiopagani82@users.noreply.github.com>
2026-08-05 23:14:35 +0200

    Initial commit

---
+++ next.config.ts
@@ -0,0 +1,15 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.ytimg.com",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
